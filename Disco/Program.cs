0046c36d58de574ba3547a15e42795ca0e013588
Peter Merrild Christensen <pete6206@edu.eal.dk>
2017-08-17 11:53:20 +0200

    Add project files.

---
+++ Disco/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
